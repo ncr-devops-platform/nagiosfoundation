4f4d1ab9416fffa6c7d2668f84d11a62fd105dc9
quaggoth <45633106+quaggoth@users.noreply.github.com>
2019-01-24 10:03:58 -0600

    Normalize lib/app with directories for all command source files

--- cmd/check_performance_counter/main.go
+++ cmd/check_performance_counter/main.go
@@ -2,7 +2,7 @@
 package main
 
 import (
-	"github.com/jkerry/nagiosfoundation/lib/app/nagiosfoundation"
+	"github.com/jkerry/nagiosfoundation/lib/app/nagiosfoundation/check_performance_counter"
 )
 
 func main() {
