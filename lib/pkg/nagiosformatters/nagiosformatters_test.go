c86f63aa3c1de0cf7f84d5d3a2ddf44727694e7f
quaggoth <45633106+quaggoth@users.noreply.github.com>
2019-02-26 11:44:58 -0600

    use a struct to drive the tests

--- lib/pkg/nagiosformatters/nagiosformatters_test.go
+++ lib/pkg/nagiosformatters/nagiosformatters_test.go
@@ -8,73 +8,89 @@ import (
 func TestNagiosFormatters(t *testing.T) {
 	const testCheck = "TestCheck"
 	const testMetric = "Test Metric"
-	const testOk = " OK"
-	const testWarning = " WARNING"
-	const testCritical = " CRITICAL"
+	const testOk = "OK"
+	const testWarning = "WARNING"
+	const testCritical = "CRITICAL"
+
+	type formatTests struct {
+		name           string
+		method         func(string, float64, float64, float64, string) (string, int)
+		expectedValue  int
+		expectedString string
+		value          float64
+		warning        float64
+		critical       float64
+	}
+
+	testList := []formatTests{
+		{
+			name:           "Greater",
+			method:         GreaterFormatNagiosCheck,
+			expectedValue:  0,
+			expectedString: testOk,
+			value:          50,
+			warning:        80,
+			critical:       90,
+		},
+		{
+			name:           "Greater",
+			method:         GreaterFormatNagiosCheck,
+			expectedValue:  1,
+			expectedString: testWarning,
+			value:          85,
+			warning:        80,
+			critical:       90,
+		},
+		{
+			name:           "Greater",
+			method:         GreaterFormatNagiosCheck,
+			expectedValue:  2,
+			expectedString: testCritical,
+			value:          95,
+			warning:        80,
+			critical:       90,
+		},
+		{
+			name:           "Lesser",
+			method:         LesserFormatNagiosCheck,
+			expectedValue:  0,
+			expectedString: testOk,
+			value:          95,
+			warning:        90,
+			critical:       80,
+		},
+		{
+			name:           "Lesser",
+			method:         LesserFormatNagiosCheck,
+			expectedValue:  1,
+			expectedString: testWarning,
+			value:          85,
+			warning:        90,
+			critical:       80,
+		},
+		{
+			name:           "Lesser",
+			method:         LesserFormatNagiosCheck,
+			expectedValue:  2,
+			expectedString: testCritical,
+			value:          50,
+			warning:        90,
+			critical:       80,
+		},
+	}
 
 	var msg string
 	var retval int
 
-	// Greater Check OK
-	msg, retval = GreaterFormatNagiosCheck(testCheck, 50, 80, 90, testMetric)
+	for _, test := range testList {
+		msg, retval = test.method(testCheck, test.value, test.warning, test.critical, testMetric)
 
-	if retval != 0 {
-		t.Error("Greater check OK failed with retval", retval)
-	}
+		if retval != test.expectedValue {
+			t.Errorf("%s check %s failed with retval %d", test.name, test.expectedString, retval)
+		}
 
-	if !strings.HasPrefix(msg, testCheck+testOk) {
-		t.Error("Greater check OK failed with msg", msg)
-	}
-
-	// Greater Check WARNING
-	msg, retval = GreaterFormatNagiosCheck(testCheck, 85, 80, 90, testMetric)
-
-	if retval != 1 {
-		t.Error("Greater check WARNING failed")
-	}
-
-	if !strings.HasPrefix(msg, testCheck+testWarning) {
-		t.Error("Greater check WARNING failed with msg", msg)
-	}
-
-	// Greater Check CRITICAL
-	msg, retval = GreaterFormatNagiosCheck(testCheck, 95, 80, 90, testMetric)
-
-	if retval != 2 {
-		t.Error("Greater check CRITICAL failed")
-	}
-
-	if !strings.HasPrefix(msg, testCheck+testCritical) {
-		t.Error("Greater check CRITICAL failed with msg", msg)
-	}
-
-	// Lesser Check OK
-	msg, retval = LesserFormatNagiosCheck(testCheck, 95, 90, 80, testMetric)
-	if retval != 0 {
-		t.Error("Lesser check OK failed")
-	}
-
-	if !strings.HasPrefix(msg, testCheck+testOk) {
-		t.Error("Lesser check OK failed with msg", msg)
-	}
-
-	// Lesser Check WARNING
-	msg, retval = LesserFormatNagiosCheck(testCheck, 85, 90, 80, testMetric)
-	if retval != 1 {
-		t.Error("Lesser check WARNING failed")
-	}
-
-	if !strings.HasPrefix(msg, testCheck+testWarning) {
-		t.Error("Lesser check WARNING failed with msg", msg)
-	}
-
-	// Lesser Check CRITICAL
-	msg, retval = LesserFormatNagiosCheck(testCheck, 50, 90, 80, testMetric)
-	if retval != 2 {
-		t.Error("Lesser check CRITICAL failed")
-	}
-
-	if !strings.HasPrefix(msg, testCheck+testCritical) {
-		t.Error("Lesser check CRITICAL failed with msg", msg)
+		if !strings.HasPrefix(msg, testCheck+" "+test.expectedString) {
+			t.Errorf("%s check %s failed with msg %s", test.name, test.expectedString, msg)
+		}
 	}
 }
