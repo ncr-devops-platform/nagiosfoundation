0b7f7eb4a83b555a1ef75dd5d25e634297166a11
quaggoth <45633106+quaggoth@users.noreply.github.com>
2019-03-21 09:52:42 -0500

    Add help via Cobra for check_cpu

--- lib/app/nagiosfoundation/check_cpu_test.go
+++ lib/app/nagiosfoundation/check_cpu_test.go
@@ -2,43 +2,29 @@ package nagiosfoundation
 
 import (
 	"errors"
-	"flag"
-	"os"
 	"testing"
 )
 
 func TestCheckCpu(t *testing.T) {
-	pgmName := "TestCheckCpu"
 	testReturnValid := func() (float64, error) { return 0.5, nil }
 	testReturnError := func() (float64, error) { return 0.5, errors.New("GetCPULoad() failure") }
 
-	// Save args and flagset for restoration
-	savedArgs := os.Args
-	savedFlagCommandLine := flag.CommandLine
-
 	// No "get memory" service passed
-	os.Args = []string{pgmName}
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	msg, retcode := CheckCPUWithHandler(nil)
+	msg, retcode := CheckCPUWithHandler(85, 95, "", nil)
 
 	if retcode != 2 || msg == "" {
 		t.Error("CheckCPUWithHandler() failed to handle nil service")
 	}
 
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	msg, retcode = CheckCPUWithHandler(testReturnValid)
+	msg, retcode = CheckCPUWithHandler(85, 95, "pct_processor_time", testReturnValid)
 
 	if retcode != 0 || msg == "" {
 		t.Error("CheckCPUWithHandler() failed with valid returns from service")
 	}
 
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	msg, retcode = CheckCPUWithHandler(testReturnError)
+	msg, retcode = CheckCPUWithHandler(85, 95, "pct_processor_time", testReturnError)
 
 	if retcode != 2 || msg == "" {
 		t.Error("CheckCPUWithHandler() failed with error returned from service")
 	}
-
-	os.Args = savedArgs
-	flag.CommandLine = savedFlagCommandLine
 }
