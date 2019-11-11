90abfe7237b937bd2b80f27b1e4ebb8846e78a47
quaggoth <45633106+quaggoth@users.noreply.github.com>
2019-11-11 11:31:32 -0600

    add nagios output to check_process

--- lib/app/nagiosfoundation/check_process.go
+++ lib/app/nagiosfoundation/check_process.go
@@ -138,7 +138,7 @@ func (p ProcessCheck) IsProcessRunning() bool {
 	return p.ProcessCheckHandler.IsProcessRunning(p.ProcessName)
 }
 
-func checkRunning(processCheck ProcessCheck, invert bool) (string, int) {
+func checkRunning(processCheck ProcessCheck, metricName string, invert bool) (string, int) {
 	var msg string
 	var retcode int
 	var responseStateText string
@@ -146,21 +146,25 @@ func checkRunning(processCheck ProcessCheck, invert bool) (string, int) {
 
 	result := processCheck.IsProcessRunning()
 	if result != invert {
-		retcode = 0
+		retcode = statusCodeOK
 		responseStateText = statusTextOK
 	} else {
-		retcode = 2
+		retcode = statusCodeCritical
 		responseStateText = statusTextCritical
 	}
 
+	nagiosOutput := metricName + "="
 	if result == true {
 		checkInfo = ""
+		nagiosOutput = nagiosOutput + strconv.Itoa(statusCodeOK)
 	} else {
 		checkInfo = "not "
+		nagiosOutput = nagiosOutput + strconv.Itoa(statusCodeCritical)
 	}
 
 	msg, _ = resultMessage(checkProcessName, responseStateText,
-		fmt.Sprintf("Process %s is %srunning", processCheck.ProcessName, checkInfo))
+		fmt.Sprintf("Process %s is %srunning", processCheck.ProcessName, checkInfo),
+		nagiosOutput)
 
 	return msg, retcode
 }
@@ -169,7 +173,7 @@ func checkRunning(processCheck ProcessCheck, invert bool) (string, int) {
 // service for interrogating the OS for the named process.
 // This is mainly used for testing but can also be used for any
 // application wishing to override the normal interrogations.
-func checkProcessWithService(name string, checkType string, processService ProcessService) (string, int) {
+func checkProcessWithService(name, checkType, metricName string, processService ProcessService) (string, int) {
 	pc := ProcessCheck{
 		ProcessName:         name,
 		ProcessCheckHandler: processService,
@@ -180,12 +184,12 @@ func checkProcessWithService(name string, checkType string, processService Proce
 
 	switch checkType {
 	case "running":
-		msg, retcode = checkRunning(pc, false)
+		msg, retcode = checkRunning(pc, metricName, false)
 	case "notrunning":
-		msg, retcode = checkRunning(pc, true)
+		msg, retcode = checkRunning(pc, metricName, true)
 	default:
 		msg = fmt.Sprintf("Invalid check type: %s", checkType)
-		retcode = 2
+		retcode = statusCodeCritical
 	}
 
 	return msg, retcode
@@ -194,7 +198,7 @@ func checkProcessWithService(name string, checkType string, processService Proce
 // checkProcessCmd will interrogate the OS for details on
 // a named process. The details of the interrogation
 // depend on the check type.
-func checkProcessCmd(name string, checkType string, checkProcess func(string, string, ProcessService) (string, int), processService ProcessService) (string, int) {
+func checkProcessCmd(name, checkType, metricName string, checkProcess func(string, string, string, ProcessService) (string, int), processService ProcessService) (string, int) {
 	var invalidParametersMsg string
 	var msg string
 	var retcode int
@@ -212,9 +216,9 @@ func checkProcessCmd(name string, checkType string, checkProcess func(string, st
 
 	if invalidParametersMsg != "" {
 		msg, _ = resultMessage(checkProcessName, statusTextCritical, invalidParametersMsg)
-		retcode = 2
+		retcode = statusCodeCritical
 	} else {
-		msg, retcode = checkProcess(name, checkType, processService)
+		msg, retcode = checkProcess(name, checkType, metricName, processService)
 	}
 
 	return msg, retcode
@@ -222,6 +226,6 @@ func checkProcessCmd(name string, checkType string, checkProcess func(string, st
 
 // CheckProcess finds a process by name to determine
 // if it is running or not running.
-func CheckProcess(name string, checkType string) (string, int) {
-	return checkProcessCmd(name, checkType, checkProcessWithService, new(processHandler))
+func CheckProcess(name, checkType, metricName string) (string, int) {
+	return checkProcessCmd(name, checkType, metricName, checkProcessWithService, new(processHandler))
 }
--- lib/app/nagiosfoundation/check_process_test.go
+++ lib/app/nagiosfoundation/check_process_test.go
@@ -42,61 +42,61 @@ func TestCheckProcess(t *testing.T) {
 
 	var retcode int
 	// Running check with running process
-	_, retcode = checkProcessWithService(testProcessGoodName, "running", new(testProcessHandler))
-	if retcode != 0 {
+	_, retcode = checkProcessWithService(testProcessGoodName, "running", "metric", new(testProcessHandler))
+	if retcode != statusCodeOK {
 		t.Errorf("Running check with running process failed with retcode %d", retcode)
 	}
 
 	// Not running check with running process
-	_, retcode = checkProcessWithService(testProcessGoodName, "notrunning", new(testProcessHandler))
-	if retcode != 2 {
+	_, retcode = checkProcessWithService(testProcessGoodName, "notrunning", "metric", new(testProcessHandler))
+	if retcode != statusCodeCritical {
 		t.Errorf("Not running check with running process failed with retcode %d", retcode)
 	}
 
 	// Running check with not running process
-	_, retcode = checkProcessWithService(testProcessBadName, "running", new(testProcessHandler))
-	if retcode != 2 {
+	_, retcode = checkProcessWithService(testProcessBadName, "running", "metric", new(testProcessHandler))
+	if retcode != statusCodeCritical {
 		t.Errorf("Running check with not running process failed with retcode %d", retcode)
 	}
 
 	// Not running check with not running process
-	_, retcode = checkProcessWithService(testProcessBadName, "notrunning", new(testProcessHandler))
-	if retcode != 0 {
+	_, retcode = checkProcessWithService(testProcessBadName, "notrunning", "metric", new(testProcessHandler))
+	if retcode != statusCodeOK {
 		t.Errorf("Not running check with not running process failed with retcode %d", retcode)
 	}
 
 	// Invalid check type
-	_, retcode = checkProcessWithService(testProcessGoodName, "", new(testProcessHandler))
-	if retcode != 2 {
+	_, retcode = checkProcessWithService(testProcessGoodName, "", "metric", new(testProcessHandler))
+	if retcode != statusCodeCritical {
 		t.Errorf("Invalid check type not detected with retcode %d", retcode)
 	}
 
 	testMsg := "Test Message"
-	testCheckProcess := func(name string, checkType string, processService ProcessService) (string, int) {
-		return testMsg, 0
+	testCheckProcess := func(name, checkType, metricName string, processService ProcessService) (string, int) {
+		return testMsg, statusCodeOK
 	}
 
-	_, retcode = checkProcessCmd("dummyprocess", "running", testCheckProcess, new(testProcessHandler))
+	_, retcode = checkProcessCmd("dummyprocess", "running", "metric", testCheckProcess, new(testProcessHandler))
 
-	if retcode != 0 {
+	if retcode != statusCodeOK {
 		t.Error("valid check process test should have returned OK")
 	}
 
-	_, retcode = checkProcessCmd("", "dummytype", testCheckProcess, new(testProcessHandler))
+	_, retcode = checkProcessCmd("", "dummytype", "metric", testCheckProcess, new(testProcessHandler))
 
-	if retcode != 2 {
+	if retcode != statusCodeCritical {
 		t.Error("check process with no -name should return CRITICAL")
 	}
 
-	_, retcode = checkProcessCmd("", "", testCheckProcess, new(testProcessHandler))
+	_, retcode = checkProcessCmd("", "", "metric", testCheckProcess, new(testProcessHandler))
 
-	if retcode != 2 {
+	if retcode != statusCodeCritical {
 		t.Error("check process test with no parameters should have returned CRITICAL")
 	}
 
-	_, retcode = checkProcessCmd("dummyprocess", "badtype", testCheckProcess, new(testProcessHandler))
+	_, retcode = checkProcessCmd("dummyprocess", "badtype", "metric", testCheckProcess, new(testProcessHandler))
 
-	if retcode != 2 {
+	if retcode != statusCodeCritical {
 		t.Error("check process test with invalid type should have returned CRITICAL")
 	}
 }
