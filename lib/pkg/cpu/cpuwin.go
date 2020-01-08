6ee56652f946bb2d389e95cd79eabbb7a54ca97c
John Kerry <jk185160@ncr.com>
2020-01-08 13:51:48 -0500

    moving the CPU check to direct WMI query

--- lib/pkg/cpu/cpuwin.go
+++ lib/pkg/cpu/cpuwin.go
@@ -3,13 +3,19 @@
 package cpu
 
 import (
-	"github.com/ncr-devops-platform/nagiosfoundation/lib/pkg/perfcounters"
+	"github.com/StackExchange/wmi"
 )
 
+type Win32_PerfFormattedData_PerfOS_Processor struct {
+	PercentProcessorTime				uint64
+}
+
 func getCPULoadOsConstrained() (float64, error) {
-	counter, err := perfcounters.ReadPerformanceCounter("\\Processor(_Total)\\% Processor Time", 2, 1)
-	if err == nil {
-		return counter.Value, nil
+	var dst []Win32_PerfFormattedData_PerfOS_Processor
+	q := wmi.CreateQuery(&dst, "WHERE name='_Total'")
+	err := wmi.Query(q, &dst)
+	if(err != nil){
+		return 0.0, err
 	}
-	return 0.0, err
+	return (float64)(dst[0].PercentProcessorTime), nil
 }
