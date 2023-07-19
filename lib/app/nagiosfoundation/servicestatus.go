92b3ccbd0d48fe2377698080d378e2fffeef409e
Thomas Kent <tk185166@ncr.com>
2023-07-19 09:20:59 -0400

    set retcode to 2 for current_state flag when service DNE

--- lib/app/nagiosfoundation/servicestatus.go
+++ lib/app/nagiosfoundation/servicestatus.go
@@ -97,13 +97,10 @@ func (i *serviceInfo) ProcessInfo() (string, int) {
 
 	if !i.IsName(i.desiredName) {
 		if i.currentStateWanted {
-			checkInfo = fmt.Sprintf("%s does not exist", i.desiredName)
 			nagiosInfo = fmt.Sprintf("%s=255 service_name=%s", i.metricName, i.desiredName)
-			retcode = 0 // change this to retcode = 2?
-		} else {
-			checkInfo = fmt.Sprintf("%s does not exist", i.desiredName)
-			retcode = 2
 		}
+		checkInfo = fmt.Sprintf("%s does not exist", i.desiredName)
+		retcode = 2
 	} else if i.currentStateWanted {
 		checkInfo = fmt.Sprintf("%s is in a %s state", i.desiredName, i.ActualStateText())
 		nagiosInfo = fmt.Sprintf("%s=%d service_name=%s", i.metricName, i.ActualStateNbr(), i.desiredName)
